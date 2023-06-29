f49712baeb2f82e0eb3365c90fe044ee827b06e5
DupeaJ <127266733+DupeaJ@users.noreply.github.com>
2023-06-29 00:05:53 -0700

    annotated javascript file, fixed restart button and function, let through the css for my timer

--- assets/script.js
+++ assets/script.js
@@ -44,120 +44,114 @@ var quizQuestions = [
 
 const startQuizButton = document.getElementById("start-quiz");
 const timerElement = document.getElementById("timer");
-const submitButton = document.getElementById("submit");
+const restartButton = document.getElementById("restart");
 const questionContainer = document.getElementById("question-container");
 const optionList = document.getElementById("option-list");
 const feedbackText = document.getElementById("feedback-text");
 
-let timer = 60;
-let intervalId = null;
-let score = 0;
-let currentQuestionIndex = 0;
+let timer = 60; //timer is set to 60 constantant
+let intervalId = null; //lets variable be empty until uppdated
+let score = 0; //score equals 0 and updates per correct answer
+let currentQuestionIndex = 0; //sets currentQuestionIndex to start at 0 and we can update it to another question
 
-function updateTimer() {
+function updateTimer() {//timer function, timer starts at constant 60 then updates minus 1 each time its called
   timer--;
-  timerElement.textContent = timer;
+  timerElement.textContent = timer;//selecting ID of timer, calling it and adding text to it in which the cuurent time on the timer is displayed
 
-  if (timer === 0) {
+  if (timer === 0) { //when timer hits 0 the endquiz function and stoptimer function stop.
     stopTimer();
     endQuiz();
   }
 }
 
-function startTimer() {
+function startTimer() { //timer start function calls update timer function every 1000 miliseconds
   timerElement.textContent = timer;
   intervalId = setInterval(updateTimer, 1000);
 }
 
-function stopTimer() {
+function stopTimer() {//stop timer function with clearInterval
   clearInterval(intervalId);
 }
 
-function endQuiz() {
+function endQuiz() { //endquiz function to hide questions and button
   questionContainer.style.display = "none";
-  submitButton.style.display = "none";
+  restartButton.style.display = "block";
 
 
-  localStorage.setItem("quizScore", score);
+  localStorage.setItem("quizScore", score); //grabs quiz score and stores it locally under score
 
 
-  window.open("./assets/HighScores.html", "_blank");
+  window.open("./assets/HighScores.html", "_blank"); //when quiz is over opens new tab with scores stored in local
 
-  const restartButton = document.createElement("button");
-  restartButton.textContent = "Restart Quiz";
-  restartButton.addEventListener("click", restartQuiz);
-  questionContainer.appendChild(restartButton);
 }
 
-function checkAnswer(selectedOption) {
-  const question = quizQuestions[currentQuestionIndex];
+function checkAnswer(selectedOption) { //defines checkAnswer under the selected question
+  const question = quizQuestions[currentQuestionIndex]; //the number the index is on is the question
 
-  if (selectedOption === question.correctAnswer) {
-    score++;
-    feedbackText.textContent = "Correct!";
-    feedbackText.classList.add("correct");
-  } else {
+  if (selectedOption === question.correctAnswer) { //if user inputed option is equal to the current questions correct answer 
+    score++;//then the score will be updated to +1 of current value of score
+    feedbackText.textContent = "Correct!";//places a nice correct string text
+    feedbackText.classList.add("correct");//adds element correct list and displays it
+  } else {//or else if the answer was wrong it minus's 10 seconds of the timers current time
     timer -= 10;
-    timer = Math.max(timer, 0);
-    timerElement.textContent = timer;
-    feedbackText.textContent = "Wrong!";
+    timer = Math.max(timer, 0); //stops timer from going below 0
+    timerElement.textContent = timer;//specifies to use current time
+    feedbackText.textContent = "Wrong!";//adds list and text saying wrong
     feedbackText.classList.add("wrong");
   }
 
-  optionList.innerHTML = "";
+  optionList.innerHTML = "";//removes text/question
 
-  if (currentQuestionIndex < quizQuestions.length - 1) {
-    currentQuestionIndex++;
-    showQuestion();
-  } else {
+  if (currentQuestionIndex < quizQuestions.length - 1) { //determines if current index is less than the length of the questions -1 
+     currentQuestionIndex++;//update to next question in index
+    showQuestion();//repeat show function unless all questions are answered
+  } else {//then it stops the timer and quiz
     stopTimer();
     endQuiz();
   }
 }
 
-function showQuestion() {
-  const question = quizQuestions[currentQuestionIndex];
-  const options = question.options;
+function showQuestion() { //updates show Question 
+  const question = quizQuestions[currentQuestionIndex];//const of question is the current question in index
+  const options = question.options;//options equal all the options for answering the question
 
-  questionContainer.innerHTML = "";
-  optionList.innerHTML = "";
+  questionContainer.innerHTML = ""; //clearstext of questions
+  optionList.innerHTML = "";//clears text of options
 
-  const questionElement = document.createElement("h2");
+  const questionElement = document.createElement("h2"); //creates element h2 and assign the question to it in string form
   questionElement.textContent = question.question;
-  questionContainer.appendChild(questionElement);
+  questionContainer.appendChild(questionElement);//puts it onto the page in the questions container
 
-  options.forEach((option) => {
+  options.forEach((option) => { //similar to the last but where making a list of the available options 
     const optionItem = document.createElement("li");
-    optionItem.textContent = option;
-    optionItem.addEventListener("click", () => checkAnswer(option));
-    optionList.appendChild(optionItem);
+    optionItem.textContent = option; 
+    optionItem.addEventListener("click", () => checkAnswer(option));//adds event listener onto option items with click function and we call to check the answer of which option clicked
+    optionList.appendChild(optionItem);//appends it to the page
   });
-  return;
+  
 }
-
-
 
 function startQuiz() {
-  startQuizButton.style.display = "none";
-  questionContainer.style.display = "block";
-  submitButton.style.display = "block";
+  startQuizButton.style.display = "none";//starting quiz removes button 
+  questionContainer.style.display = "block";//sets container to block
+  restartButton.style.display = "none";//changes restart button to hide
 
-  showQuestion();
-  startTimer();
+  showQuestion();//starts questions
+  startTimer();//starts timer
 
 }
-function restartQuiz() {
- 
-  score = 0;
+
+function restartQuiz() {//defines the restart function 
+  score = 0;//sets all elements back to default no matter where youre at
   currentQuestionIndex = 0;
   timer = 60;
-  feedbackText.textContent = "";
-  timerElement.textContent = "";
+  feedbackText.textContent = "";//empties the correct or wrong text
+  timerElement.textContent = "";//emptys the timer
 
-  questionContainer.innerHTML = "";
-  optionList.innerHTML = "";
+  questionContainer.innerHTML = "";//empties questions
+  optionList.innerHTML = "";//empties options
 
-  startQuiz();
+  startQuiz(); //starts up startQuiz again
 }
-
-startQuizButton.addEventListener("click", startQuiz);
+restartButton.addEventListener("click", restartQuiz);//adds click funstion to restart quiz when u click restart, then calls the restatQuiz function
+startQuizButton.addEventListener("click", startQuiz);//adds click event to The start quiz button, then runs the startQuiz function
