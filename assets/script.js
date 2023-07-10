4085ee532afb44c4db45728d5edd4a4b44d4df3c
DupeaJ <127266733+DupeaJ@users.noreply.github.com>
2023-07-09 20:16:37 -0700

    node_modules

--- assets/script.js
+++ assets/script.js
@@ -55,6 +55,15 @@ let score = 0; //score equals 0 and updates per correct answer
 let scoreTime = 0; //setting up recorder for time left on quiz
 let currentQuestionIndex = 0; //sets currentQuestionIndex to start at 0 and we can update it to another question
 
+function startQuiz() {
+  startQuizButton.style.display = "none";//starting quiz removes button 
+  questionContainer.style.display = "block";//sets container to block
+  restartButton.style.display = "none";//changes restart button to hide
+
+  showQuestion();//starts questions
+  startTimer();//starts timer
+
+}
 function updateTimer() {//timer function, timer starts at constant 60 then updates minus 1 each time its called
   timer--;
   timerElement.textContent = timer;//selecting ID of timer, calling it and adding text to it in which the cuurent time on the timer is displayed
@@ -75,14 +84,39 @@ function stopTimer() {//stop timer function with clearInterval
 }
 
 function endQuiz() { //endquiz function to hide questions and button
-  questionContainer.style.display = "none";
+  // questionContainer.style.display = "none";
   restartButton.style.display = "block";
   
   localStorage.setItem("quizScore", score); //grabs quiz score and stores it locally under score
   localStorage.setItem("timeLeft", scoreTime);
   
-  
-  window.open("./assets/HighScores.html", "_blank"); //when quiz is over opens new tab with scores stored in local
+  let promptini = prompt("please enter initials...", "harry potter");
+  localStorage.setItem("initials", promptini);
+
+  const storedScore = localStorage.getItem("quizScore");
+  const storedTimeLeft = localStorage.getItem("timeLeft");
+  const storedInintialsid = localStorage.getItem("initials");
+  if (storedInintialsid) {
+      const scoreList = document.getElementById("score-list");
+      const scoreItem = document.createElement("li");
+      scoreItem.textContent = "initials: " + storedInintialsid;
+      scoreList.appendChild(scoreItem);
+  }
+
+  if (storedScore) {
+      const scoreList = document.getElementById("score-list");
+      const scoreItem = document.createElement("li");
+      scoreItem.textContent = "Score: " + storedScore;
+      scoreList.appendChild(scoreItem);
+  }
+
+  if (storedTimeLeft) {
+      const scoreList = document.getElementById("score-list");
+      const scoreItem = document.createElement("li");
+      scoreItem.textContent = "Time: " + storedTimeLeft;
+      scoreList.appendChild(scoreItem);
+  }
+  // window.open("./assets/HighScores.html", "_blank"); //when quiz is over opens new tab with scores stored in local
   
 }
 
@@ -91,6 +125,7 @@ function checkAnswer(selectedOption) { //defines checkAnswer under the selected 
 
   if (selectedOption === question.correctAnswer) { //if user inputed option is equal to the current questions correct answer 
     score++;//then the score will be updated to +1 of current value of score
+    scoreTime = timer;
     feedbackText.textContent = "Correct!";//places a nice correct string text
     feedbackText.classList.add("correct");//adds element correct list and displays it
   } else {//or else if the answer was wrong it minus's 10 seconds of the timers current time
@@ -134,17 +169,14 @@ function showQuestion() { //updates show Question
   
 }
 
-function startQuiz() {
-  startQuizButton.style.display = "none";//starting quiz removes button 
-  questionContainer.style.display = "block";//sets container to block
-  restartButton.style.display = "none";//changes restart button to hide
 
-  showQuestion();//starts questions
-  startTimer();//starts timer
 
-}
+
+
+
 
 function restartQuiz() {//defines the restart function 
+  localStorage.removeItem("timeLeft");
   score = 0;//sets all elements back to default no matter where youre at
   currentQuestionIndex = 0;
   timer = 60;
@@ -156,5 +188,8 @@ function restartQuiz() {//defines the restart function
 
   startQuiz(); //starts up startQuiz again
 }
+
+
+
 restartButton.addEventListener("click", restartQuiz);//adds click funstion to restart quiz when u click restart, then calls the restatQuiz function
 startQuizButton.addEventListener("click", startQuiz);//adds click event to The start quiz button, then runs the startQuiz function
--- assets/scripty.js
+++ assets/scripty.js
@@ -1,27 +1,27 @@
-let promptini = prompt("please enter initials...", "harry potter");
-localStorage.setItem("initials", promptini);
+// let promptini = prompt("please enter initials...", "harry potter");
+// localStorage.setItem("initials", promptini);
 
 
-const storedScore = localStorage.getItem("quizScore");
-const storedTimeLeft = localStorage.getItem("timeLeft");
-const storedInintialsid = localStorage.getItem("initials");
-if (storedInintialsid) {
-    const scoreList = document.getElementById("score-list");
-    const scoreItem = document.createElement("li");
-    scoreItem.textContent = "initials: " + storedInintialsid;
-    scoreList.appendChild(scoreItem);
-}
+// const storedScore = localStorage.getItem("quizScore");
+// const storedTimeLeft = localStorage.getItem("timeLeft");
+// const storedInintialsid = localStorage.getItem("initials");
+// if (storedInintialsid) {
+//     const scoreList = document.getElementById("score-list");
+//     const scoreItem = document.createElement("li");
+//     scoreItem.textContent = "initials: " + storedInintialsid;
+//     scoreList.appendChild(scoreItem);
+// }
 
-if (storedScore) {
-  const scoreList = document.getElementById("score-list");
-  const scoreItem = document.createElement("li");
-  scoreItem.textContent = "Score: " + storedScore;
-  scoreList.appendChild(scoreItem);
-}
+// if (storedScore) {
+//   const scoreList = document.getElementById("score-list");
+//   const scoreItem = document.createElement("li");
+//   scoreItem.textContent = "Score: " + storedScore;
+//   scoreList.appendChild(scoreItem);
+// }
 
-if (storedTimeLeft) {
-    const scoreList = document.getElementById("score-list");
-    const scoreItem = document.createElement("li");
-    scoreItem.textContent = "Time: " + storedTimeLeft;
-    scoreList.appendChild(scoreItem);
-}
+// if (storedTimeLeft) {
+//     const scoreList = document.getElementById("score-list");
+//     const scoreItem = document.createElement("li");
+//     scoreItem.textContent = "Time: " + storedTimeLeft;
+//     scoreList.appendChild(scoreItem);
+// }
